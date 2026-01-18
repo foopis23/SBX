ff7ed85151ec1aa6c6a9531faffe43c59cb597fc
Eric Sexton <foopis23@gmail.com>
2026-01-18 15:24:25 -0500

    move example to 1 file to make it more sharable

--- apps/examples/examples/physics/traits.ts
+++
@@ -1,5 +0,0 @@
-import { trait } from 'koota';
-import { OrbitControls as ThreeOrbitControls } from 'three/addons/controls/OrbitControls.js';
-
-export const OrbitControls = trait<() => ThreeOrbitControls>(() => null!)
-
